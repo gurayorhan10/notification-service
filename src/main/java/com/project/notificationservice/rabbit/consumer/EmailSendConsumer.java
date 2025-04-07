0a30b7ce219510d0c6eeb2b908ab5380991a831d
gurayorhan10 <gurayorhan10@gmail.com>
2025-04-07 17:53:27 +0300

    Update EmailSendConsumer.java

--- src/main/java/com/project/notificationservice/rabbit/consumer/EmailSendConsumer.java
+++ src/main/java/com/project/notificationservice/rabbit/consumer/EmailSendConsumer.java
@@ -1,11 +1,9 @@
 package com.project.notificationservice.rabbit.consumer;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.project.notificationservice.rabbit.data.EmailSendMessage;
 import com.project.notificationservice.rabbit.queue.EmailSendQueue;
 import com.project.notificationservice.service.EmailService;
-import jakarta.mail.MessagingException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -22,10 +20,14 @@ public class EmailSendConsumer {
 
     @Transactional(rollbackFor = Exception.class)
     @RabbitListener(queues = EmailSendQueue.QUEUE_NAME)
-    public void consumer(Message message) throws JsonProcessingException, MessagingException {
-        log.info(new String(message.getBody()));
-        EmailSendMessage emailSendMessage = new ObjectMapper().readValue(new String(message.getBody()), EmailSendMessage.class);
-        emailService.send(emailSendMessage.getTo(),emailSendMessage.getTitle(),emailSendMessage.getContent(),emailSendMessage.getSimple());
+    public void consumer(Message message) {
+        try{
+            log.info(new String(message.getBody()));
+            EmailSendMessage emailSendMessage = new ObjectMapper().readValue(new String(message.getBody()), EmailSendMessage.class);
+            emailService.send(emailSendMessage.getTo(),emailSendMessage.getTitle(),emailSendMessage.getContent(),emailSendMessage.getSimple());
+        }catch (Exception e){
+            log.error(e.getLocalizedMessage());
+        }
     }
 
 }
